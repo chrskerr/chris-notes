db23250876ab8ffc707434c856085ebe32e95de8
Chris Kerr <chriskerr@me.com>
2022-10-12 08:55:39 +1000

    improved component memoisation

--- app/components/category.tsx
+++ app/components/category.tsx
@@ -8,6 +8,7 @@ import type {
 } from 'react';
 import { memo } from 'react';
 import { useEffect, useState } from 'react';
+import isEqual from 'fast-deep-equal';
 
 interface Props {
 	category: {
@@ -43,114 +44,117 @@ const handleKeyUp: KeyboardEventHandler<HTMLSpanElement> = e => {
 	}
 };
 
-export const Category = memo(function Category({ category, refetch }: Props) {
-	const { id, title, notes } = category;
+export const Category = memo(
+	function Category({ category, refetch }: Props) {
+		const { id, title, notes } = category;
 
-	const [titleContent, setTitleContent] = useState(title);
-	const [isDraggedOver, setIsDraggedOver] = useState(false);
+		const [titleContent, setTitleContent] = useState(title);
+		const [isDraggedOver, setIsDraggedOver] = useState(false);
 
-	useEffect(() => {
-		setTitleContent(title);
-	}, [title]);
+		useEffect(() => {
+			setTitleContent(title);
+		}, [title]);
 
-	useEffect(() => {
-		if (titleContent !== title) {
-			debouncedHandleSave(id, titleContent, refetch);
-		}
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [titleContent]);
+		useEffect(() => {
+			if (titleContent !== title) {
+				debouncedHandleSave(id, titleContent, refetch);
+			}
+			// eslint-disable-next-line react-hooks/exhaustive-deps
+		}, [titleContent]);
 
-	function handleAdd() {
-		fetch('/api/create/note', {
-			method: 'post',
-			body: JSON.stringify({ id: id }),
-		}).then(refetch);
-	}
-
-	const handleDragOver: DragEventHandler<HTMLDetailsElement> = e => {
-		e.preventDefault();
-		setIsDraggedOver(true);
-	};
-
-	const handleDrop: DragEventHandler<HTMLDetailsElement> = e => {
-		setIsDraggedOver(false);
-		const noteId = e.dataTransfer.getData('text/plain');
-		fetch('/api/edit/note', {
-			method: 'post',
-			body: JSON.stringify({ id: noteId, categoryId: id }),
-			credentials: 'include',
-		}).then(refetch);
-	};
-
-	function handleDelete() {
-		const shouldProceed = confirm(
-			`Are you sure you want to delete "${titleContent}"?`,
-		);
-		if (shouldProceed) {
-			fetch('/api/delete/category', {
+		function handleAdd() {
+			fetch('/api/create/note', {
 				method: 'post',
-				body: JSON.stringify({ id }),
-				credentials: 'include',
+				body: JSON.stringify({ id: id }),
 			}).then(refetch);
 		}
-	}
 
-	const handleToggle: MouseEventHandler<HTMLDetailsElement> = e => {
-		fetch('/api/edit/category', {
-			method: 'post',
-			body: JSON.stringify({ id, isOpen: e.currentTarget.open }),
-			credentials: 'include',
-		});
-	};
+		const handleDragOver: DragEventHandler<HTMLDetailsElement> = e => {
+			e.preventDefault();
+			setIsDraggedOver(true);
+		};
 
-	return (
-		<details
-			className={`p-1 pl-4 mb-4 border border-dashed rounded transition-color ${
-				isDraggedOver ? 'border-black' : ''
-			}`}
-			open={category.isOpen}
-			onToggle={handleToggle}
-			onDragEnter={handleDragOver}
-			onDragOver={handleDragOver}
-			onDragLeave={() => setIsDraggedOver(false)}
-			onDrop={handleDrop}
-		>
-			<summary className="flex items-center">
-				<div className="flex items-center flex-1 stransition-color rounded hover:bg-slate-100 [&:has(*:focus)]:bg-slate-100">
-					<span
-						className="flex-1 px-4 py-2 text-xl outline-none"
-						contentEditable
-						suppressContentEditableWarning
-						spellCheck
-						onInput={e =>
-							setTitleContent(e.currentTarget.innerText)
-						}
-						onKeyDown={handleKeyDown}
-						onKeyUp={handleKeyUp}
-						onClick={e => e.preventDefault()}
-					>
-						{title}
-					</span>
-					<span
-						onClick={handleDelete}
-						className="mr-4 text-red-500 cursor-pointer"
-					>
-						x
-					</span>
-				</div>
-			</summary>
-			<div className="pl-[17px]">
-				{notes.map(note => (
-					<Note key={note.id} note={note} refetch={refetch} />
-				))}
-			</div>
-			<div
-				onClick={handleAdd}
-				className="px-4 py-2 mt-2 transition-colors rounded cursor-pointer hover:bg-slate-100"
+		const handleDrop: DragEventHandler<HTMLDetailsElement> = e => {
+			setIsDraggedOver(false);
+			const noteId = e.dataTransfer.getData('text/plain');
+			fetch('/api/edit/note', {
+				method: 'post',
+				body: JSON.stringify({ id: noteId, categoryId: id }),
+				credentials: 'include',
+			}).then(refetch);
+		};
+
+		function handleDelete() {
+			const shouldProceed = confirm(
+				`Are you sure you want to delete "${titleContent}"?`,
+			);
+			if (shouldProceed) {
+				fetch('/api/delete/category', {
+					method: 'post',
+					body: JSON.stringify({ id }),
+					credentials: 'include',
+				}).then(refetch);
+			}
+		}
+
+		const handleToggle: MouseEventHandler<HTMLDetailsElement> = e => {
+			fetch('/api/edit/category', {
+				method: 'post',
+				body: JSON.stringify({ id, isOpen: e.currentTarget.open }),
+				credentials: 'include',
+			});
+		};
+
+		return (
+			<details
+				className={`p-1 pl-4 mb-4 border border-dashed rounded transition-color ${
+					isDraggedOver ? 'border-black' : ''
+				}`}
+				open={category.isOpen}
+				onToggle={handleToggle}
+				onDragEnter={handleDragOver}
+				onDragOver={handleDragOver}
+				onDragLeave={() => setIsDraggedOver(false)}
+				onDrop={handleDrop}
 			>
-				<span className="mr-2">+</span>
-				<span>Add new note</span>
-			</div>
-		</details>
-	);
-});
+				<summary className="flex items-center">
+					<div className="flex items-center flex-1 stransition-color rounded hover:bg-slate-100 [&:has(*:focus)]:bg-slate-100">
+						<span
+							className="flex-1 px-4 py-2 text-xl outline-none"
+							contentEditable
+							suppressContentEditableWarning
+							spellCheck
+							onInput={e =>
+								setTitleContent(e.currentTarget.innerText)
+							}
+							onKeyDown={handleKeyDown}
+							onKeyUp={handleKeyUp}
+							onClick={e => e.preventDefault()}
+						>
+							{title}
+						</span>
+						<span
+							onClick={handleDelete}
+							className="mr-4 text-red-500 cursor-pointer"
+						>
+							x
+						</span>
+					</div>
+				</summary>
+				<div className="pl-[17px]">
+					{notes.map(note => (
+						<Note key={note.id} note={note} refetch={refetch} />
+					))}
+				</div>
+				<div
+					onClick={handleAdd}
+					className="px-4 py-2 mt-2 transition-colors rounded cursor-pointer hover:bg-slate-100"
+				>
+					<span className="mr-2">+</span>
+					<span>Add new note</span>
+				</div>
+			</details>
+		);
+	},
+	(prevProps, newProps) => isEqual(newProps.category, prevProps.category),
+);
--- app/components/done.tsx
+++ app/components/done.tsx
@@ -4,6 +4,8 @@ import { memo } from 'react';
 import { format } from 'date-fns';
 import { utcToZonedTime } from 'date-fns-tz';
 
+import isEqual from 'fast-deep-equal';
+
 interface Props {
 	data: UINote[];
 	refetch: () => void;
@@ -26,30 +28,39 @@ function categoriseNotesByDate(
 	return [...doneMap.values()];
 }
 
-export const Done = memo(function Done({ data, refetch }: Props) {
-	if (data.length === 0) {
-		return null;
-	}
+export const Done = memo(
+	function Done({ data, refetch }: Props) {
+		if (data.length === 0) {
+			return null;
+		}
 
-	const categorisedData = categoriseNotesByDate(data);
+		const categorisedData = categoriseNotesByDate(data);
 
-	return (
-		<details className="p-1 pl-4 mb-4 border border-dashed rounded">
-			<summary className="flex items-center">
-				<span className="flex-1 px-4 py-2 text-xl outline-none">
-					Done
-				</span>
-			</summary>
-			<div className="pl-[17px] pt-2">
-				{categorisedData.map(noteGroup => (
-					<details key={noteGroup.label} className="mb-3">
-						<summary className="pb-1">{noteGroup.label}</summary>
-						{noteGroup.notes.map(note => (
-							<Note key={note.id} note={note} refetch={refetch} />
-						))}
-					</details>
-				))}
-			</div>
-		</details>
-	);
-});
+		return (
+			<details className="p-1 pl-4 mb-4 border border-dashed rounded">
+				<summary className="flex items-center">
+					<span className="flex-1 px-4 py-2 text-xl outline-none">
+						Done
+					</span>
+				</summary>
+				<div className="pl-[17px] pt-2">
+					{categorisedData.map(noteGroup => (
+						<details key={noteGroup.label} className="mb-3">
+							<summary className="pb-1">
+								{noteGroup.label}
+							</summary>
+							{noteGroup.notes.map(note => (
+								<Note
+									key={note.id}
+									note={note}
+									refetch={refetch}
+								/>
+							))}
+						</details>
+					))}
+				</div>
+			</details>
+		);
+	},
+	(prevProps, newProps) => isEqual(newProps.data, prevProps.data),
+);
