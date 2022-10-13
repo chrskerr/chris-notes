5d178a58ad6eeb51cf89d00beed7ba8aa3a757e8
Chris Kerr <chriskerr@me.com>
2022-10-13 18:21:42 +1000

    reverted "fix"

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,14 +1,4 @@
 import { RemixBrowser } from '@remix-run/react';
-import React from 'react';
 import { hydrateRoot } from 'react-dom/client';
 
-requestIdleCallback(() => {
-	React.startTransition(() => {
-		hydrateRoot(
-			document,
-			<React.StrictMode>
-				<RemixBrowser />
-			</React.StrictMode>,
-		);
-	});
-});
+hydrateRoot(document, <RemixBrowser />);
