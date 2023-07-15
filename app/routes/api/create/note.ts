025c2919f52fd35cf4303880bb9a6dc291e6bbd2
Chris Kerr <chriskerr@me.com>
2023-07-16 08:07:37 +1000

    new notes are created empty

--- app/routes/api/create/note.ts
+++ app/routes/api/create/note.ts
@@ -6,7 +6,7 @@ export const action: ActionFunction = async ({ request }) => {
 	const body: { id?: string } = await request.json();
 	if (!body.id) return null;
 
-	await db.note.create({ data: { categoryId: body.id } });
+	await db.note.create({ data: { categoryId: body.id, content: '' } });
 
 	return null;
 };
